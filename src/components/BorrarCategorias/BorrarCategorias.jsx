8432c2403af9f0ca828a8d7cf4ed421479045659
JuanP-VT <cv.juanp@gmail.com>
2022-10-03 17:00:29 -0600

    ADD- BorrarCategorias component

--- src/components/BorrarCategorias/BorrarCategorias.jsx
+++ src/components/BorrarCategorias/BorrarCategorias.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import {fetchAllCategorias} from '../../store/slices/CategoriasSlice/'
 import {useDispatch, useSelector} from 'react-redux'
+import { Button, Card, Image, Grid } from 'semantic-ui-react'
 const BorrarCategorias = () => {
   const {list} = useSelector(state => state.categorias)
   const dispatch = useDispatch();
@@ -8,8 +9,30 @@ const BorrarCategorias = () => {
     dispatch(fetchAllCategorias())
   },[dispatch])
   return (
-    <div>BorrarCategorias</div>
+   <>
+   <Card.Group>
+    {list.map((elem,index)=>(   <Card>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src={elem.imgUrl}
+        />
+        <Card.Header>{elem.name}</Card.Header>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='red'>
+            Delete
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>))}
+    </Card.Group>
+   </>
   )
 }
 
 export default BorrarCategorias
+
+
