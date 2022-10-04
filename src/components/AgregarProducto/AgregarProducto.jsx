c69dc7da8b396e96fbe5b131214bff8327f45cff
JuanP-VT <cv.juanp@gmail.com>
2022-10-04 10:02:34 -0600

    ADD- Client Validacion para AgregarProductos

--- src/components/AgregarProducto/AgregarProducto.jsx
+++ src/components/AgregarProducto/AgregarProducto.jsx
@@ -1,10 +1,12 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import { Button, Form, Message, Select,Label } from 'semantic-ui-react'
 import {fetchAllCategorias} from '../../store/slices/CategoriasSlice'
 import {useDispatch, useSelector} from 'react-redux'
 import AgregarProductoRequest from './AgregarProductoRequest'
 
 const AgregarProducto = () => {
+  // Este hook es para guardar mensajes de acuerdo al estado del llenado del form
+  const [UIMessage,setUIMessage] = useState('')
     //Global App state para el listado de las categorías
   const {list} = useSelector(state => state.categorias)
   // El selector de semantic ui requiere un array de objectos con las propiedades key, value y text
@@ -16,15 +18,21 @@ const AgregarProducto = () => {
     dispatch(fetchAllCategorias())
   },[dispatch])
   return (
-     <Form error required success >
+     <Form error success >
     <Form.Input label='Nombre del producto' placeholder='Nombre' id='productName'/>
     <Form.Field>
           <Label pointing='below'>Seleccione la categoría</Label>
     <Select placeholder='Categoria' options={selectList} id ='productCategoria'/>
+    
     </Form.Field>
     <Form.Input label='Unidades disponibles' placeholder='Unidades disponibles' id='productUnidadesDisponibles' type='number'/>
     <Form.Input label='Precio' placeholder='Precio' id='productPrecio' type='number'/>
-    <Button onClick={(e)=>AgregarProductoRequest(e)}>Agregar</Button>
+     <Message
+      error
+      header='Error'
+      content={UIMessage}
+    />
+    <Button onClick={(e)=>AgregarProductoRequest(e,setUIMessage)}>Agregar</Button>
   </Form>
   )
 }
--- src/components/AgregarProducto/AgregarProductoRequest.js
+++ src/components/AgregarProducto/AgregarProductoRequest.js
@@ -1,9 +1,31 @@
-export default async function AgregarProductoRequest(e){
+export default async function AgregarProductoRequest(e,setMessage){
     //Extraemos los valores de los inputs
     const name = document.querySelector('#productName').value
-    const categoria = document.querySelector('#productCategoria').firstChild.textContent
+    let categoria = document.querySelector('#productCategoria').firstChild.textContent
     const stock = document.querySelector('#productUnidadesDisponibles').value
     const precio = document.querySelector('#productPrecio').value
+    //Client validation
+    if(name.length < 3){
+        setMessage('El nombre debe contener al menos 3 caracteres')
+        return;
+    }
+         if(stock === ''){
+        setMessage('Especifique la cantidad de unidades disponibles')
+        return;
+    }
+    //por default si no se selecciona categoría el valor obtenido debe ser sin categoria
+    if (categoria === 'Categoria'){
+        categoria = 'sin categoria'
+    }
+     if(precio === ''){
+        setMessage('Especifique el precio del producto')
+        return;
+    }
+     if(precio<= 0){
+        setMessage('El precio debe ser mayor a 0')
+        return;
+    }
     //Creamos nueva entry
     const newEntry = {name:name, categoria:categoria,unidadesDisponibles:stock,precio:precio}
+    console.log(newEntry)
 }
